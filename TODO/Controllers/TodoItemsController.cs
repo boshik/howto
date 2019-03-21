24c0658f38a9b59a73b5e50506e70e38f5126c17
Aliaksandr Martsineuski <a.martsineuski@godeltech.com>
2019-03-21 13:08:32 +0300

    project cleanup with .net core update

--- TODO/Controllers/TodoItemsController.cs
+++ TODO/Controllers/TodoItemsController.cs
@@ -1,44 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TODO.Controllers
+namespace HOWTO.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [ApiController]
+    [Route("api/todo-items")]
+    public class TodoItemsController : Controller
     {
-        // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public Task<IActionResult> Get()
         {
-            return new string[] { "value1", "value2" };
+            throw new NotImplementedException();
+            return Task.FromResult((IActionResult) new EmptyResult());
         }
 
-        // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            throw new NotImplementedException();
         }
 
-        // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            throw new NotImplementedException();
         }
 
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            throw new NotImplementedException();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            throw new NotImplementedException();
         }
     }
 }
